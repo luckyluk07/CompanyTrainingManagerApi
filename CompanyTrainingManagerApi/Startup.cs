46935a54f4a9f5bb41b3ba245c060533709503a0
luckyluk07 <L.Nojman11@wp.pl>
2021-08-20 20:57:19 +0200

    Init first connection with database

--- CompanyTrainingManagerApi/Startup.cs
+++ CompanyTrainingManagerApi/Startup.cs
@@ -1,7 +1,9 @@
+using CompanyTrainingManagerApi.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,9 @@ namespace CompanyTrainingManagerApi
         {
 
             services.AddControllers();
+
+            //making db connection
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
